3db040687b14d8ef5341d609eb73fe0b81441d0c
targetakhil <targetakhil@gmail.com>
2021-11-12 13:50:27 +0530

    update plot code

--- rangesGraph.py
+++ rangesGraph.py
@@ -1,11 +1,15 @@
-import csv
+import json
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
 from os import listdir
 import os
 
-def readCsv():
+SEQ = 0
+PAR = 1
+RANGES = 2
+
+def generateBenchmark():
     labelToCol = {
         'seq' : 'red',
         'par' : 'blue',
@@ -13,38 +17,39 @@ def readCsv():
     }
 
     folder_path = 'C:\\Users\\targe\Documents\\ranges_csv\\ranges_benchmarks'
-    folders = listdir(folder_path + '\\csv_files')
+    folders = listdir(folder_path + '\\json_files')
 
     for scan_file in folders:
         fig, ax = plt.subplots()
+        bm_data = {}
+        bm_data[SEQ] = []
+        bm_data[PAR] = []
+        bm_data[RANGES] = []
 
+        n = 0
+
+        scan_file_path = folder_path + '\\json_files\\' + scan_file
+        with open(scan_file_path) as jsonfile:
+            data = json.load(jsonfile)
+
+            for bm in data['benchmarks']:
+                bm_data[bm['family_index']].append(bm)
+
+        n = len(bm_data[SEQ])
         inputSizes = []
         seqTime = []
         parTime = []
-        rangesTime = []
-
         parRelative = []
         rangesRelative = []
+        rangeTime = []
 
-        scan_file_path = folder_path + '\\csv_files\\' + scan_file
-        with open(scan_file_path) as csvfile:
-            valueReader = csv.reader(csvfile)
-            first = True
-            c = 5
-            for row in valueReader:
-                N = float(row[0])
-                seq = float(row[1])
-                par = float(row[2])
-                ranges = float(row[3])
-
-                inputSizes.append(c)
-                c += 1
-                seqTime.append(seq)
-                parTime.append(par)
-                rangesTime.append(ranges)
-
-                parRelative.append(seq/par)
-                rangesRelative.append(seq/ranges)
+        for i in range(0, n):
+            inputSizes.append(i + 5)
+            seqTime.append(int(bm_data[SEQ][i]['real_time']))
+            parTime.append(int(bm_data[PAR][i]['real_time']))
+            rangeTime.append(int(bm_data[RANGES][i]['real_time']))
+            parRelative.append(float(bm_data[SEQ][i]['real_time']) / float(bm_data[PAR][i]['real_time']))
+            rangesRelative.append(float(bm_data[SEQ][i]['real_time']) / float(bm_data[RANGES][i]['real_time']))
         
         ax.set_xticks(range(5, 32, 1))
 
@@ -65,7 +70,7 @@ def readCsv():
         handles.append(mpatches.Patch(color='white', label="L3 Cache - 16 MiB"))
 
         plt.legend(loc="upper left", handles=handles)
-        plt.savefig(folder_path + '\\plots\\' + os.path.splitext(scan_file)[0] + '.png')
+        plt.savefig(folder_path + '\\new_plots\\' + os.path.splitext(scan_file)[0] + '.png')
 
 if __name__ == "__main__":
-    readCsv()
+    generateBenchmark()
